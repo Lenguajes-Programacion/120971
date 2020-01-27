57667890d5c35c864866c8135d47b379680019a1
Daniel Zaragoza <60015713+danielzaragoza@users.noreply.github.com>
2020-01-27 12:15:46 -0700

    subiendo el programa practica 1
    creando de nuevo el archivo practica uno con argumentos y condiciones if else

---
+++ clase_1/Practica1/practica1/practica1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace practica1
+{
+    class Program
+    {
+        //variable de la clase-
+         static string nombre = "Daniel";
+
+        static void Main(string[] args)
+        {
+            //respondemos con el primer argumento.
+            //lenght significa longitud en este caso el tamaño de la caden el doble && significa otra comparacion extra y se debe cumplir las 2 para que acepte el iff el argumento.
+            //comparaciones logicas  : >< ,  == , >= , <=  !=
+            if (args[0].Length > 0 && args[0] == nombre)//comparacion de boolean verdadeo o falso.
+            {
+                Console.WriteLine($"hello {args[0]} {args[1]}");
+            }
+            else
+            {
+                Console.WriteLine("usuario no reconocido");
+            }
+        }
+    }
+}
