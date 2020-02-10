f4fce5a43ec4dd239e05cd044d6dd87b5a5a2b80
Daniel Zaragoza <60015713+danielzaragoza@users.noreply.github.com>
2020-02-10 12:23:56 -0700

    avance practica calculadora
    calculadora debe tener un ciclo while, para cerrar la aplicacion, hacer las suma,resta, multi,division, seguido de poder usar el mismo resultado para hacer otra operacion.

---
+++ clase_1/practica2/practica2calculadora/practica2calculadora/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace practica2calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salir = null;
+            while (salir != "e")
+            {
+                Console.WriteLine("calculadora");
+                Console.WriteLine("elija una opcion de las siguientes");
+                Console.WriteLine("a suma");
+                Console.WriteLine("b resta");
+                Console.WriteLine("c multiplicacion");
+                Console.WriteLine("d division");
+                Console.WriteLine("1 salir");
+
+
+
+                
+                string opcion;
+                int valor1 = 0;
+                int valor2 = 0;
+                int resultado = 0;
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "a":
+                        Console.WriteLine("ingresa el primer valor");
+                        valor1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("ingresa el primer valor");
+                        valor2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("el resultado es {0} \n", resultado);
+                        break;
+                    case "b":
+                        break;
+                    case "c":
+                        break;
+                    case "d":
+                        break;
+                    default:
+                        Console.WriteLine("ha ingresado una opcion no valida, por favor intente de nuevbo");
+                        break;
+                }
+            }
+
+        }
+    }
+    class calculadora
+    {
+        
+        static int suma(int valor1, int valor2) {
+            int resultado = 0;
+            resultado = valor1 + valor2;
+            return resultado;
+        }
+        static int resta(int valor1, int valor2)
+        {
+            int resultado = 0;
+            resultado = valor1 + valor2;
+            return resultado;
+        }
+        static int multi(int valor1, int valor2)
+        {
+            int resultado = 0;
+            resultado = valor1 + valor2;
+            return resultado;
+        }
+        static int division(int valor1, int valor2)
+        {
+            int resultado = 0;
+            resultado = valor1 + valor2;
+            return resultado;
+        }
+    }
+}
