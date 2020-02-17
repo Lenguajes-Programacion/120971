0cfd3c7b2c9d4932ddf485f8625e52b0afca4b67
Daniel Zaragoza <60015713+danielzaragoza@users.noreply.github.com>
2020-02-16 18:31:31 -0700

    tarea de la calculadora funcionando
    calculadora terminada funcionando hasta con do while, para visualizarlo favor de crear un nuevo proyecto

---
+++ clase_1/Practica1/practica1/practica2/calculadora funcional.cs
@@ -0,0 +1,102 @@
+using System;
+
+namespace practica2calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion="";
+            do{
+            {
+                Console.WriteLine("calculadora");
+                Console.WriteLine("elija una opcion de las siguientes");
+                Console.WriteLine("a suma");
+                Console.WriteLine("b resta");
+                Console.WriteLine("c multiplicacion");
+                Console.WriteLine("d division");
+                Console.WriteLine("e salir");
+
+
+
+                
+                
+                int valor1 = 0;
+                int valor2 = 0;
+                int resultado = 0;
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "a":
+                        Console.WriteLine("ingresa el primer valor");
+                        valor1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("ingresa el segundo valor");
+                        valor2 = int.Parse(Console.ReadLine());
+                        resultado = calculadora.suma(valor1, valor2);
+                        Console.WriteLine("el resultado es {0} \n", resultado);
+                        break;
+                    case "b":
+                        Console.WriteLine("ingresa el primer valor");
+                        valor1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("ingresa el segundo valor");
+                        valor2 = int.Parse(Console.ReadLine());
+                        resultado = calculadora.resta(valor1, valor2);
+                        Console.WriteLine("el resultado es {0} \n", resultado);
+                        break;
+                    case "c":
+                        Console.WriteLine("ingresa el primer valor");
+                        valor1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("ingresa el segundo valor");
+                        valor2 = int.Parse(Console.ReadLine());
+                        resultado = calculadora.multi(valor1, valor2);
+                        Console.WriteLine("el resultado es {0} \n", resultado);
+                        break;
+                        Console.WriteLine("ingresa el primer valor");
+                        valor1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("ingresa el segundo valor");
+                        valor2 = int.Parse(Console.ReadLine());
+                        resultado = calculadora.division(valor1, valor2);
+                        Console.WriteLine("el resultado es {0} \n", resultado);
+                    case "d":
+                        break;
+                    default:
+                        Console.WriteLine("ha ingresado una opcion no valida, por favor intente de nuevbo");
+                        break;
+                }
+            }
+           }while (opcion!="e");
+        }
+    }
+    class calculadora
+    {
+        
+        public static int suma(int valor1, int valor2) {
+            int resultado = 0;
+            resultado = valor1 + valor2;
+            return resultado;
+        }
+        public static int resta(int valor1, int valor2)
+        {
+            int resultado = 0;
+            resultado = valor1 - valor2;
+            return resultado;
+        }
+         public static int multi(int valor1, int valor2)
+        {
+            int resultado = 0;
+            resultado = valor1 * valor2;
+            return resultado;
+        }
+        public static int division(int valor1, int valor2)
+        {
+            int resultado = 0;
+            resultado = valor1 / valor2;
+            return resultado;
+        }
+    
+    }
+}
