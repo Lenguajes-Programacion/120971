909aca82f18efc26e56a89b7fe7e00641005ad7e
Daniel Zaragoza <60015713+danielzaragoza@users.noreply.github.com>
2020-02-21 12:48:35 -0700

    memoria en json mejorado
    avance de memoria en json mejoradp

--- entrega con solucion/entrega1proyecto/Class1.cs
+++ entrega con solucion/entrega1proyecto/Class1.cs
@@ -12,10 +12,19 @@ namespace entrega1proyecto
             StreamReader reader = new StreamReader(archivoDB);
             var dbJSON = reader.ReadToEnd();
             var dbObject = JObject.Parse(dbJSON);
-            var result = dbObject.ToString();
-            Console.WriteLine(result);
+            //prueba de lectura con json
+           // var result = dbObject.ToString();
+            //Console.WriteLine(result);
+            //ejemplo con foreach para ver datos
+            foreach(var item in dbObject)// var item, convertir un item en db object y este se guarde para despues mostrarlo 
+            {
+                Console.WriteLine("Dato en memoria:");
+                Console.WriteLine(item.ToString());
+            }
             //fundamentos de un json se necesita un indice, json es una estructura de "key": "value", el value puede ser cualquier cosa string, int, etc.
             //cada parentesis de un json es un nivel de acceso.
+            //arreglos  son asi [], los archivos json no necesitan indices, en caso del arreglo, requiere un indice para poder acceder a los datos
+            //foreach busqueda avanzada de datos
         }
     }
 }
