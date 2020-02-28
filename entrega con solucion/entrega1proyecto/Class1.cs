b2330092825cb553d57fccba8ad1defe9884de4a
Daniel Zaragoza <60015713+danielzaragoza@users.noreply.github.com>
2020-02-28 12:42:46 -0700

    correciones
    falta implementar el archivo de lectura y escritura json

--- entrega con solucion/entrega1proyecto/Class1.cs
+++ entrega con solucion/entrega1proyecto/Class1.cs
@@ -20,7 +20,7 @@ namespace entrega1proyecto
             foreach ((var key, var item) in dbObject)
             {
                 Console.WriteLine("Dato en memoria:");
-                MemoriaData memoriaData = new MemoriaData(DateTime.Today, item["operacion"].ToString(),(int) item["resultado"]);// el int despues de item significa que lo esta convirtiendo en un int
+                MemoriaData memoriaData = new MemoriaData(DateTime.Today, item["operacion"].ToString(), (int)item["resultado"]);// el int despues de item significa que lo esta convirtiendo en un int
                 DateTime convertir = DateTime.Parse(key);
                 Console.WriteLine(convertir);//parse analiza cadenas y lo convierte a lo que se desea https://www.kyocode.com/2019/06/metodo-parse-clase-convert-c/
                 //Console.WriteLine( key.ToString());
@@ -29,22 +29,23 @@ namespace entrega1proyecto
                 Console.WriteLine("resultado de la operacion : ");
                 //var resultadop = memoriaData.resultado;
                 //resultadop = Convert.ToInt32(resultadop);
-                 //Console.WriteLine(resultadop);
-                Console.WriteLine( memoriaData.resultado.ToString());
-                
+                //Console.WriteLine(resultadop);
+                Console.WriteLine(memoriaData.resultado.ToString());
+
             }
             //fundamentos de un json se necesita un indice, json es una estructura de "key": "value", el value puede ser cualquier cosa string, int, etc.
             //cada parentesis de un json es un nivel de acceso.
             //arreglos  son asi [], los archivos json no necesitan indices, en caso del arreglo, requiere un indice para poder acceder a los datos
             //foreach busqueda avanzada de datos
         }
+
     }
     class MemoriaData
     {
         public DateTime fecha;
         public String operacion;
         public int resultado;
-        public MemoriaData(DateTime date,String operation, int result)
+        public MemoriaData(DateTime date, String operation, int result)
         {
             fecha = date;
             operacion = operation;
@@ -54,3 +55,5 @@ namespace entrega1proyecto
 
     }
 }
+
+//investigar que son las listas en c# y que tienen que ver(relacion) con los arreglos. subir en pdf o archivo de github para el lunes.
