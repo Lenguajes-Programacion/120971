54b6591a4b497c372e62aa61244f9eb9fd078e43
Daniel Zaragoza <60015713+danielzaragoza@users.noreply.github.com>
2020-03-01 18:37:06 -0700

    correcciones
    correciones, limpiando

--- entrega con solucion/entrega1proyecto/Class1.cs
+++ entrega con solucion/entrega1proyecto/Class1.cs
@@ -1,71 +1,12 @@
 ﻿using Newtonsoft.Json;
-using System.IO;
 using System;
-
-
+using System.IO;
 
 namespace entrega1proyecto
 {
-<<<<<<< HEAD
-    /* class memoria
-     {
-         public void LeerMemoria()
-         {
-             string archivoDB = "../../../db.json";
-             StreamReader reader = new StreamReader(archivoDB);
-             var dbJSON = reader.ReadToEnd();
-             var dbObject = JObject.Parse(dbJSON);
-             //prueba de lectura con json
-             // var result = dbObject.ToString();
-             //Console.WriteLine(result);
-             //ejemplo con foreach para ver datos
-             //foreach(var item in dbObject) var item, convertir un item en db object y este se guarde para despues mostrarlo 
-             foreach ((var key, var item) in dbObject)
-             {
-                 Console.WriteLine("Dato en memoria:");
-                 MemoriaData memoriaData = new MemoriaData(DateTime.Today, item["operacion"].ToString(),(int) item["resultado"]);// el int despues de item significa que lo esta convirtiendo en un int
-                 DateTime convertir = DateTime.Parse(key);
-                 Console.WriteLine(convertir);//parse analiza cadenas y lo convierte a lo que se desea https://www.kyocode.com/2019/06/metodo-parse-clase-convert-c/
-                 //Console.WriteLine( key.ToString());
-                 Console.WriteLine("operacion realizada:");
-                 Console.WriteLine(memoriaData.operacion);
-                 Console.WriteLine("resultado de la operacion : ");
-                 //var resultadop = memoriaData.resultado;
-                 //resultadop = Convert.ToInt32(resultadop);
-                  //Console.WriteLine(resultadop);
-                 Console.WriteLine( memoriaData.resultado.ToString());
-
-             }
-             //fundamentos de un json se necesita un indice, json es una estructura de "key": "value", el value puede ser cualquier cosa string, int, etc.
-             //cada parentesis de un json es un nivel de acceso.
-             //arreglos  son asi [], los archivos json no necesitan indices, en caso del arreglo, requiere un indice para poder acceder a los datos
-             //foreach busqueda avanzada de datos
-         }
-     }
-     class MemoriaData
-     {
-         public DateTime fecha;
-         public String operacion;
-         public int resultado;
-         public MemoriaData(DateTime date,String operation, int result)
-         {
-             fecha = date;
-             operacion = operation;
-             resultado = result;
-
-         }
-
-     }*/
-
-    class escribirjson 
-    {
-        public int indice;
-        public int res;
-        //programamenu.Calculadora resultadog = new programamenu.Calculadora( );
-        public void escribir()
-=======
     class memoria
     {
+        /*
         public void LeerMemoria()
         {
             string archivoDB = "../../../db.json";
@@ -106,12 +47,10 @@ namespace entrega1proyecto
         public String operacion;
         public int resultado;
         public MemoriaData(DateTime date, String operation, int result)
->>>>>>> 02b39463c19cb88ab35210a70a144823191ae95d
         {
-            escribirjson guardar = new escribirjson { indice = 1, res=0 };
-            String outputJson = JsonConvert.SerializeObject(guardar);
-            File.WriteAllText("../../../db.json", outputJson);
-            Console.WriteLine("escribiendo en json");
+            fecha = date;
+            operacion = operation;
+            resultado = result;
 
         }
 
@@ -129,6 +68,25 @@ namespace entrega1proyecto
             resultado = resultados;
         }
     }
+    */
+    }
+
+    class escribirjson
+    {
+        public int indice;
+        public int res;
+
+        public void escribir()
+        {
+            escribirjson guardar = new escribirjson { indice = 1, res = 0 };
+            String outputJson = JsonConvert.SerializeObject(guardar);
+            File.WriteAllText("../../../db.json", outputJson);
+            Console.WriteLine("escribiendo en json");
+
+        }
+    }
 }
+       
+ 
 
 //investigar que son las listas en c# y que tienen que ver(relacion) con los arreglos. subir en pdf o archivo de github para el lunes.
